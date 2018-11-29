08552fbda8e3c43e775a21b3bde6ca66fde26f85
Marc GERINI <mgerini@MacBook-Pro-de-Marc.local>
2018-11-29 11:48:12 +0100

    Remove HTTP Offload

--- lb.tf
+++ lb.tf
@@ -5,11 +5,10 @@ resource "openstack_lb_loadbalancer_v2" "lb1" {
 }
 
 resource "openstack_lb_listener_v2" "listener_https" {
-  name                      = "listener_https"
-  protocol                  = "TERMINATED_HTTPS"
-  protocol_port             = 443
-  loadbalancer_id           = "${openstack_lb_loadbalancer_v2.lb1.id}"
-  default_tls_container_ref = "https://osp13lab.jaguar-network.com:13311/v1/containers/81fe1175-7da5-4c15-8018-3e7b3dfb4196"
+  name            = "listener_https"
+  protocol        = "HTTP"
+  protocol_port   = 443
+  loadbalancer_id = "${openstack_lb_loadbalancer_v2.lb1.id}"
 }
 
 resource "openstack_lb_pool_v2" "pool1" {
